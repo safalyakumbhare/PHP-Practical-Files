b7f36f0ef23dedc1cded0ac18ebae787ee6a34b5
safalyakumbhare <safalyakumbhare@gmail.com>
2023-10-16 20:33:21 +0530

    Sort Array Of Integers

---
+++ Practical 11.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>
+            Practical 11
+        </title>
+    </head>
+    <body>
+        
+        <?php
+
+        $num = array(50,40,10,20,30);
+        sort($num);
+        
+        for($i=0;$i<=4;$i++)
+        {
+            echo $num[$i];
+            echo "<br>";
+        }
+        
+        ?>
+    </body>
+</html>
