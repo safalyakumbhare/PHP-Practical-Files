0d557e021f29385d04a0fbe816b566719385a80c
safalyakumbhare <safalyakumbhare@gmail.com>
2023-10-19 19:51:42 +0530

    Cookie

---
+++ Practical 12.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set("India");
+echo "The current date and time is: ";
+echo date("Y-m-d h:i:sa");
+?>
---
+++ Practical 13.php
@@ -0,0 +1,20 @@
+<?php
+    $_COOKIE_name = "User";
+    $_COOKIE_value = "Aspire";
+
+    setcookie($_COOKIE_name,$_COOKIE_value,time() + 86400,"/");
+    echo "Cookie Created";
+    echo "<hr>";
+?>
+<html>
+    <body>
+        <?php
+            echo "Reading Cookie :- ";
+            echo "<br>";
+            echo "The Cookie Name is ".$_COOKIE_name;
+            echo "<br>";
+            echo "The Cookie Value is ".$_COOKIE_value;
+
+        ?>
+    </body>
+</html>
