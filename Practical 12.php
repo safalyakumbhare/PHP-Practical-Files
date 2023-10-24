35de0a299a995a6299e55139019f3c49a4dd60ea
safalyakumbhare <safalyakumbhare@gmail.com>
2023-10-24 21:49:01 +0530

    Date And Time

---
+++ Practical 12.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Practical 12</title>
+    </head>
+    <body>
+    <?php
+$date  = date('Y-m-d');
+$time = date('H:i:s');
+echo "Current Date is : $date";
+echo "<br>";
+echo "Current Time is : $time";
+?>
+    </body>
+</html>
+
