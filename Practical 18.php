6b19b47bc99766ac33bd9bf6317f7430d1847ac0
safalyakumbhare <safalyakumbhare@gmail.com>
2023-11-14 19:45:11 +0530

    Retrive Record into Database

--- Practical 18.php
+++ Practical 18.php
@@ -14,4 +14,9 @@ $sql = "SELECT * FROM `emp01`";
 $num = mysqli_query($conn,$sql);
 
 echo "There are ",mysqli_num_rows($num)," Number of Rows in the Table ";
+echo "<br>";
+while($prnt = mysqli_fetch_assoc($num)){
+    echo "ID ",$prnt['id']," Name ",$prnt['Name']," On Post of ",$prnt['Post']," With Salary of ",$prnt['salary'];
+    echo "<br>";
+}
 ?>
